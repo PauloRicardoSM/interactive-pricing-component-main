603f7c30a68d43774af04769151f8bbbce14b6d1
Paulo Ricardo <pauloricardosm@alu.ufc.br>
2024-05-14 15:14:31 -0300

    I’ve completed the JavaScript functions for the slider.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,11 @@
+//Do theslider change span tag text#money
 const slider = document.getElementById('mySlider');
 const moneySpan = document.getElementById('money');
 
 slider.oninput = function() {
+    //Changes the text of thespan#money
     moneySpan.textContent = this.value;
+    
+    //Changes the background color of the slider
+    this.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) ${this.value}%, hsl(224, 65%, 95%) ${this.value}%)`
 }
