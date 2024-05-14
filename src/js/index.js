17a438c69b3f0e11df615d4c82551568d887af8d
Paulo Ricardo <pauloricardosm@alu.ufc.br>
2024-05-14 18:07:33 -0300

    I've fixed the slider and the tags screened to it.

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,19 @@
 //Do theslider change span tag text#money
-const slider = document.getElementById('mySlider');
-const moneySpan = document.getElementById('money');
+const slider = document.getElementById('mySlider')
+const moneySpan = document.getElementById('money')
+const sliderValue = document.getElementById('sliderValue')
+const values = ['10k', '50k', '100k', '500k', '1M']
+const prices = ['8', '12', '16', '24', '36']
+
+//Changes the text of thespan#money    
+slider.addEventListener('input', function(){
+    sliderValue.textContent = `${values[this.value]} Pageviews`
+    moneySpan.textContent = `$${prices[this.value]}.00`
+
+})
 
 slider.oninput = function() {
-    //Changes the text of thespan#money
-    moneySpan.textContent = `$${this.value}.00`;
-    
     //Changes the background color of the slider
-    this.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) ${this.value}%, hsl(224, 65%, 95%) ${this.value}%)`
+    this.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) ${(this.value * 25)}%, hsl(224, 65%, 95%) ${(this.value * 25)}%)`
 }
+
